56cb37b1074e29b304425ed6f0c65b8b9acd3185
Xinyi Han <xinyi.han98@outlook.com>
2022-11-22 21:02:53 +1100

    Initial Commit

---
+++ 24-Swap-Nodes-in-Pairs.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        node = head.next
+        head.next = self.swapPairs(head.next.next)
+        node.next = head
+        return node
