cf9f8802d23de7838612936ce47986accaf67237
Xinyi Han <xinyi.han98@outlook.com>
2022-11-22 22:40:45 +1100

    Initial Commit

---
+++ 27-Remove-Element.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        j = 0
+        while j < len(nums):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+            j += 1
+        return i
