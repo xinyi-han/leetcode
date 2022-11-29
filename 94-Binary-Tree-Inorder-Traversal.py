ec6defdbfa4e146ee511b9d57f5376ba3e1f714c
Xinyi Han <xinyi.han98@outlook.com>
2022-11-29 19:28:00 +1100

    Initial Commit

---
+++ 94-Binary-Tree-Inorder-Traversal.py
@@ -0,0 +1,24 @@
+from typing import Optional, List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        output = list()
+
+        def traverse(node: Optional[TreeNode]):
+            if node is None:
+                return
+            traverse(node.left)
+            output.append(node.val)
+            traverse(node.right)
+
+        traverse(root)
+        return output
