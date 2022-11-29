27a6cbc4fac2a74e01f5b5cac7f815f390527bec
Xinyi Han <xinyi.han98@outlook.com>
2022-11-29 20:17:40 +1100

    Initial Commit

---
+++ 96-Unique-Binary-Search-Trees.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        cache = [0 for i in range(n + 1)]
+        cache[0] = 1
+        cache[1] = 1
+        for i in range(2, n + 1):
+            num = 0
+            for j in range(1, i + 1):
+                num += cache[j - 1] * cache[i - j]
+            cache[i] = num
+        return cache[-1]
