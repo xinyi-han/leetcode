72e78935964a15c1952d0a60c95316f702bd3647
Xinyi Han <xinyi.han98@outlook.com>
2022-12-05 14:36:49 +1100

    Initial Commit

---
+++ 130-Surrounded-Regions.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        m = len(board)
+        n = len(board[0])
+
+        def dfs(r: int, c: int):
+            if r < 0 or r == m or c < 0 or c == n or board[r][c] != "O":
+                return
+            board[r][c] = "T"
+            dfs(r - 1, c)
+            dfs(r + 1, c)
+            dfs(r, c - 1)
+            dfs(r, c + 1)
+
+        for j in range(n):
+            if board[0][j] == "O":
+                dfs(0, j)
+            if board[m - 1][j] == "O":
+                dfs(m - 1, j)
+        for i in range(1, m - 1):
+            if board[i][0] == "O":
+                dfs(i, 0)
+            if board[i][n - 1] == "O":
+                dfs(i, n - 1)
+
+        for r in range(m):
+            for c in range(n):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "T":
+                    board[r][c] = "O"
