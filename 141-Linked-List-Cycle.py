7b4a540a845bd47904abf242c56fe731cc401b1e
Xinyi Han <xinyi.han98@outlook.com>
2022-12-06 22:13:00 +1100

    Initial Commit

---
+++ 141-Linked-List-Cycle.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        nodes = set()
+        while head is not None:
+            if head in nodes:
+                return True
+            nodes.add(head)
+            head = head.next
+        return False
