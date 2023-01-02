c10166b96f6ea7495b73cd07ec42747080eb3fb7
Xinyi Han <xinyi.han98@outlook.com>
2023-01-02 13:54:32 +1100

    Initial Commit

---
+++ 559-Maximum-Depth-of-N-ary-Tree.py
@@ -0,0 +1,15 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if root is None:
+            return 0
+        depth = 0
+        for child in root.children:
+            depth = max(depth, self.maxDepth(child))
+        return depth + 1
