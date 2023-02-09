0fff917f0faa3b6245b44d904f88ef0598b0811b
Xinyi Han <xinyi.han98@outlook.com>
2023-02-09 15:25:32 +1100

    2023.02.09

--- 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
@@ -5,18 +5,17 @@ class TreeNode:
         self.left = None
         self.right = None
 
-
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if p.val > q.val:
             p, q = q, p
 
-        def traverse(node: 'TreeNode', i: 'TreeNode', j: 'TreeNode') -> 'TreeNode':
-            if i.val <= node.val <= j.val:
+        def find(node: TreeNode, x: TreeNode, y: TreeNode) -> TreeNode:
+            if x.val <= node.val <= y.val:
                 return node
-            elif node.val > j.val:
-                return traverse(node.left, i, j)
-            elif node.val < i.val:
-                return traverse(node.right, i, j)
+            if node.val > y.val:
+                return find(node.left, x, y)
+            elif node.val < x.val:
+                return find(node.right, x, y)
 
-        return traverse(root, p, q)
+        return find(root, p, q)
