a998e024b49ae983f1ba5915af1250ac03744cf8
Xinyi Han <xinyi.han98@outlook.com>
2023-02-09 14:07:26 +1100

    2023.02.09

--- 100-Same-Tree.py
+++ 100-Same-Tree.py
@@ -13,8 +13,8 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if p is None and q is None:
             return True
-        elif p is None or q is None:
+        if p is None or q is None:
             return False
-        elif p.val != q.val:
+        if p.val != q.val:
             return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
