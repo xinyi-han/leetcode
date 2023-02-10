4720981fb6cc46b0af35fe99082f80ef0ff9721e
Xinyi Han <xinyi.han98@outlook.com>
2023-02-11 10:01:26 +1100

    2023.02.11

--- 79-Word-Search.py
+++ 79-Word-Search.py
@@ -1,32 +1,31 @@
-from typing import List, Tuple
+from typing import List
 
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m = len(board)
         n = len(board[0])
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        pos = set()
+        visit = set()
 
-        def dfs(prev: Tuple[int, int], i: int) -> bool:
+        def dfs(i: int, x: int, y: int) -> bool:
             if i == len(word):
                 return True
-            for dx, dy in directions:
-                x, y = prev
-                x += dx
-                y += dy
-                if 0 <= x < m and 0 <= y < n and (x, y) not in pos and board[x][y] == word[i]:
-                    pos.add((x, y))
-                    if dfs((x, y), i + 1):
-                        return True
-                    pos.remove((x, y))
+            if not 0 <= x < m or not 0 <= y < n or board[x][y] != word[i] or (x, y) in visit:
+                return False
+            visit.add((x, y))
+            if dfs(i + 1, x - 1, y):
+                return True
+            if dfs(i + 1, x + 1, y):
+                return True
+            if dfs(i + 1, x, y - 1):
+                return True
+            if dfs(i + 1, x, y + 1):
+                return True
+            visit.remove((x, y))
             return False
 
         for r in range(m):
             for c in range(n):
-                if board[r][c] == word[0]:
-                    pos.add((r, c))
-                    if dfs((r, c), 1):
-                        return True
-                    pos.remove((r, c))
+                if dfs(0, r, c):
+                    return True
         return False
