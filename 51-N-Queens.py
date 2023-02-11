9059f4ef15b3fb5de066fbbef58b250988d85087
Xinyi Han <xinyi.han98@outlook.com>
2023-02-11 12:51:30 +1100

    2023.02.11

--- 51-N-Queens.py
+++ 51-N-Queens.py
@@ -1,46 +1,31 @@
-from typing import List, Tuple
+from typing import List
 
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        columns = set()
-        diagonal_pos = set()
-        diagonal_neg = set()
         output = list()
+        board = [["." for _ in range(n)] for _ in range(n)]
+        col = set()
+        diagP = set() # r + c
+        diagN = set() # r - c
 
-        def dfs(stack: List[Tuple[int, int]]):
-            if len(stack) == n:
-                board = list()
-                for r, c in stack:
-                    s = "." * c + "Q" + (n - c - 1) * "."
-                    board.append(s)
-                output.append(board)
+        def dfs(r: int):
+            if r == n:
+                result = ["".join(row) for row in board]
+                output.append(result)
                 return
-            r = len(stack)
             for c in range(n):
-                if c in columns:
+                if c in col or (r + c) in diagP or (r - c) in diagN:
                     continue
-                elif r - c in diagonal_neg:
-                    continue
-                elif r + c in diagonal_pos:
-                    continue
-                else:
-                    stack.append((r, c))
-                    columns.add(c)
-                    diagonal_neg.add(r - c)
-                    diagonal_pos.add(r + c)
-                    dfs(stack)
-                    stack.pop()
-                    columns.remove(c)
-                    diagonal_neg.remove(r - c)
-                    diagonal_pos.remove(r + c)
+                col.add(c)
+                diagP.add(r + c)
+                diagN.add(r - c)
+                board[r][c] = "Q"
+                dfs(r + 1)
+                col.remove(c)
+                diagP.remove(r + c)
+                diagN.remove(r - c)
+                board[r][c] = "."
 
-        for i in range(n):
-            columns.add(i)
-            diagonal_neg.add(0 - i)
-            diagonal_pos.add(0 + i)
-            dfs([(0, i)])
-            columns.remove(i)
-            diagonal_neg.remove(0 - i)
-            diagonal_pos.remove(0 + i)
+        dfs(0)
         return output
