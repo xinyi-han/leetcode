3fede14b177c682020ba8cf25688f0247800591b
Xinyi Han <xinyi.han98@outlook.com>
2023-02-12 12:46:51 +1100

    2023.02.12

--- 473-Matchsticks-to-Square.py
+++ 473-Matchsticks-to-Square.py
@@ -3,34 +3,32 @@ from typing import List
 
 class Solution:
     def makesquare(self, matchsticks: List[int]) -> bool:
-        sum = 0
-        maxLen = 0
+        total = 0
+        maxStick = 0
         hashMap = dict()
         for stick in matchsticks:
-            sum += stick
-            maxLen = max(maxLen, stick)
+            total += stick
+            maxStick = max(maxStick, stick)
             hashMap[stick] = hashMap.get(stick, 0) + 1
-        length = sum // 4
-        if sum % 4 != 0 or maxLen > length:
+        length = total // 4
+        if total % 4 != 0 or maxStick > length:
             return False
         sticks = list(hashMap.keys())
         sticks.sort(reverse=True)
 
-        def dfs(edgeLen: int, edgeNum: int, i: int) -> bool:
+        def dfs(edgeSum: int, edgeNum: int, i: int) -> bool:
             if edgeNum == 4:
                 return True
-            for j in range(i, len(sticks)):
-                stick = sticks[j]
+            for j, stick in enumerate(sticks[i:]):
                 if hashMap[stick] == 0:
                     continue
-                currLen = edgeLen + stick
-                if currLen > length:
+                if edgeSum + stick > length:
                     continue
                 hashMap[stick] -= 1
-                if currLen < length:
-                    if dfs(currLen, edgeNum, j):
+                if edgeSum + stick < length:
+                    if dfs(edgeSum + stick, edgeNum, i + j):
                         return True
-                else:
+                elif edgeSum + stick == length:
                     if dfs(0, edgeNum + 1, 0):
                         return True
                 hashMap[stick] += 1
