48f20e048c678130e45e6470c164e92671704d45
Xinyi Han <xinyi.han98@outlook.com>
2023-02-13 12:16:47 +1100

    2023.02.13

--- 206-Reverse-Linked-List.py
+++ 206-Reverse-Linked-List.py
@@ -8,12 +8,32 @@ class ListNode:
         self.next = next
 
 
+# Iterative
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
-        while head is not None:
-            node = head.next
-            head.next = prev
-            prev = head
-            head = node
+        curr = head
+        while curr is not None:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
         return prev
+
+
+# Recursive
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+
+        def dfs(node: Optional[ListNode]) -> Optional[ListNode]:
+            if node is None:
+                return dummy
+            prev = dfs(node.next)
+            prev.next = node
+            return node
+
+        dfs(head)
+        if head is not None:
+            head.next = None
+        return dummy.next
