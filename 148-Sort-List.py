8b79384e17b3c78192dd0fcd5f55f96b64657405
Xinyi Han <xinyi.han98@outlook.com>
2023-02-13 15:02:47 +1100

    2023.02.13

--- 148-Sort-List.py
+++ 148-Sort-List.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import Optional
 
 
@@ -8,18 +9,52 @@ class ListNode:
         self.next = next
 
 
+# Priority Queue
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        node = head
-        vals = list()
-        while node is not None:
-            vals.append(node.val)
-            node = node.next
-        vals.sort()
-        node = head
+        nodes = list()
         i = 0
-        while i < len(vals):
-            node.val = vals[i]
-            node = node.next
+        while head is not None:
+            heapq.heappush(nodes, (- head.val, i, head))
+            head = head.next
             i += 1
-        return head
+        prev = None
+        while len(nodes) > 0:
+            _, _, node = heapq.heappop(nodes)
+            node.next = prev
+            prev = node
+        return prev
+
+
+# Merge Sort
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        dummy = ListNode(0, head)
+        slow = dummy
+        fast = dummy
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        r = self.sortList(slow.next)
+        slow.next = None
+        l = self.sortList(head)
+        return self.merge(l, r)
+
+    def merge(self, l1: Optional[ListNode], l2: Optional[ListNode]):
+        dummy = ListNode()
+        curr = dummy
+        while l1 is not None and l2 is not None:
+            if l1.val <= l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        if l1 is not None:
+            curr.next = l1
+        else:
+            curr.next = l2
+        return dummy.next
