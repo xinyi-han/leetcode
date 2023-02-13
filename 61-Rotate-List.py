7f92ccd50a0c7fa49caf60d867236f310aaefd78
Xinyi Han <xinyi.han98@outlook.com>
2023-02-13 15:44:56 +1100

    2023.02.13

--- 61-Rotate-List.py
+++ 61-Rotate-List.py
@@ -10,15 +10,25 @@ class ListNode:
 
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if head is None:
-            return None
-        nodes = list()
-        while head is not None:
-            nodes.append(head)
-            head = head.next
-        k = k % len(nodes)
+        if head is None or head.next is None:
+            return head
+        curr = head
+        i = 0
+        while curr is not None:
+            i += 1
+            curr = curr.next
+        k %= i
         if k == 0:
-            return nodes[0]
-        nodes[-(k + 1)].next = None
-        nodes[-1].next = nodes[0]
-        return nodes[-k]
+            return head
+        dummy = ListNode(0, head)
+        slow = dummy
+        fast = head
+        for j in range(k - 1):
+            fast = fast.next
+        while fast.next is not None:
+            slow = slow.next
+            fast = fast.next
+        head = slow.next
+        slow.next = None
+        fast.next = dummy.next
+        return head
