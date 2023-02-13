dca7af0d19882e54f55cab932d24d0e9ce652b66
Xinyi Han <xinyi.han98@outlook.com>
2023-02-13 12:03:55 +1100

    2023.02.13

--- 143-Reorder-List.py
+++ 143-Reorder-List.py
@@ -1,5 +1,4 @@
 from typing import Optional
-from collections import deque
 
 
 # Definition for singly-linked list.
@@ -14,21 +13,29 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        nodes = deque()
-        while head is not None:
-            nodes.append(head)
+        dummy = ListNode(0, head)
+        slow = dummy
+        fast = dummy
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        prev = None
+        curr = slow.next
+        slow.next = None
+        while curr is not None:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        curr = dummy
+        while head is not None and prev is not None:
+            curr.next = head
+            curr = curr.next
             head = head.next
-
-        def topDown(flag: int) -> Optional[ListNode]:
-            if len(nodes) == 0:
-                return None
-            if flag == 0:
-                node = nodes.popleft()
-                flag = 1
-            else:
-                node = nodes.pop()
-                flag = 0
-            node.next = topDown(flag)
-            return node
-
-        topDown(0)
+            curr.next = prev
+            curr = curr.next
+            prev = prev.next
+        if head is not None:
+            curr.next = head
+        elif prev is not None:
+            curr.next = prev
