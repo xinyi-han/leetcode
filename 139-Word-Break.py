a11fc2c75eeb39b62eb85847d509c16cd37f0ef9
Xinyi Han <xinyi.han98@outlook.com>
2023-02-14 14:34:31 +1100

    2023.02.14

--- 139-Word-Break.py
+++ 139-Word-Break.py
@@ -5,11 +5,10 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         cache = [False for _ in range(len(s) + 1)]
         cache[-1] = True
-        for i in range(1, len(s) + 1):
+        for i in range(len(s) - 1, -1, -1):
             for word in wordDict:
-                # len(s) - i + len(word) <= len(s)
-                if i >= len(word) and s[len(s) - i:len(s) - i + len(word)] == word:
-                    cache[len(s) - i] = cache[len(s) - i + len(word)]
-                if cache[len(s) - i]:
+                if s[i:i+len(word)] == word:
+                    cache[i] = cache[i+len(word)]
+                if cache[i]:
                     break
         return cache[0]
