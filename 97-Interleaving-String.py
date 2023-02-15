71a443976079fcd6ff63fbabf05b8395ab975efc
Xinyi Han <xinyi.han98@outlook.com>
2023-02-15 21:19:24 +1100

    2023.02.15

--- 97-Interleaving-String.py
+++ 97-Interleaving-String.py
@@ -4,10 +4,16 @@ class Solution:
             return False
         cache = [[False for _ in range(len(s2) + 1)] for _ in range(len(s1) + 1)]
         cache[-1][-1] = True
-        for i in range(len(s1), -1, -1):
-            for j in range(len(s2), -1, -1):
-                if i + 1 <= len(s1) and s1[i] == s3[i+j] and cache[i+1][j]:
+        for j in range(len(s2) - 1, -1, -1):
+            if s2[j:] == s3[len(s1)+j:]:
+                cache[-1][j] = True
+        for i in range(len(s1) - 1, -1, -1):
+            if s1[i:] == s3[len(s2)+i:]:
+                cache[i][-1] = True
+        for i in range(len(s1) - 1, -1, -1):
+            for j in range(len(s2) - 1, -1, -1):
+                if cache[i+1][j] and s1[i] == s3[i+j]:
                     cache[i][j] = True
-                elif j + 1 <= len(s2) and s2[j] == s3[i+j] and cache[i][j+1]:
+                elif cache[i][j+1] and s2[j] == s3[i+j]:
                     cache[i][j] = True
         return cache[0][0]
