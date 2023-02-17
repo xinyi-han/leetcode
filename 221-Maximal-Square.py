381724204a3db797517ee0e64ad6c123cd494594
Xinyi Han <xinyi.han98@outlook.com>
2023-02-17 22:58:06 +1100

    2023.02.17

--- 221-Maximal-Square.py
+++ 221-Maximal-Square.py
@@ -6,14 +6,18 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
         cache = [[0 for _ in range(n)] for _ in range(m)]
+        edgeLen = 0
         for j in range(n):
-            cache[m - 1][j] = int(matrix[m - 1][j])
-        for i in range(m):
-            cache[i][n - 1] = int(matrix[i][n - 1])
+            cache[-1][j] = int(matrix[-1][j])
+            edgeLen = max(edgeLen, cache[-1][j])
+        for i in range(m - 1):
+            cache[i][-1] = int(matrix[i][-1])
+            edgeLen = max(edgeLen, cache[i][-1])
         for i in range(m - 2, -1, -1):
             for j in range(n - 2, -1, -1):
                 if matrix[i][j] == "0":
                     cache[i][j] = 0
                 else:
-                    cache[i][j] = min(cache[i + 1][j], cache[i][j + 1], cache[i + 1][j + 1]) + 1
-        return max(list(map(max, cache)))**2
+                    cache[i][j] = 1 + min(cache[i][j+1], cache[i+1][j], cache[i+1][j+1])
+                    edgeLen = max(edgeLen, cache[i][j])
+        return edgeLen**2
