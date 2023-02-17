2d60c602038785379129ad1eca2622448e5457ed
Xinyi Han <xinyi.han98@outlook.com>
2023-02-17 22:17:33 +1100

    2023.02.17

--- 152-Maximum-Product-Subarray.py
+++ 152-Maximum-Product-Subarray.py
@@ -3,17 +3,14 @@ from typing import List
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        output = nums[0]
-        minProd = 1
-        maxProd = 1
+        output = max(nums)
+        prevMin, prevMax = 1, 1
         for num in nums:
             if num == 0:
-                output = max(output, num)
-                minProd = 1
-                maxProd = 1
+                prevMin, prevMax = 1, 1
                 continue
-            prod = [num * minProd, num * maxProd, num]
-            minProd = min(prod)
-            maxProd = max(prod)
-            output = max(output, maxProd)
+            currMin = min(prevMin * num, prevMax * num, num)
+            currMax = max(prevMin * num, prevMax * num, num)
+            output = max(output, currMax)
+            prevMin, prevMax = currMin, currMax
         return output
