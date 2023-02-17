490ac4777cc12a2f62d874ab1a480456b3b45ae7
Xinyi Han <xinyi.han98@outlook.com>
2023-02-17 21:08:19 +1100

    2023.02.17

--- 300-Longest-Increasing-Subsequence.py
+++ 300-Longest-Increasing-Subsequence.py
@@ -5,7 +5,9 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         cache = [1 for _ in nums]
         for i in range(len(nums) - 2, -1, -1):
-            temp = [cache[j] for j in range(i + 1, len(nums)) if nums[j] > nums[i]]
-            maxLen = max(temp) if len(temp) > 0 else 0
-            cache[i] = max(cache[i], 1 + maxLen)
+            maxLen = 0
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    maxLen = max(maxLen, cache[j])
+            cache[i] += maxLen
         return max(cache)
