f15e93cf04d50463623a65ed7b674818531a322c
Xinyi Han <xinyi.han98@outlook.com>
2023-02-18 11:56:17 +1100

    2023.02.18

--- 45-Jump-Game-II.py
+++ 45-Jump-Game-II.py
@@ -3,23 +3,11 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        jumps = [float('inf') for _ in nums]
-        jumps[0] = 0
-        for i, num in enumerate(nums):
-            for j in range(1, num + 1):
-                if i + j < len(nums):
-                    jumps[i + j] = min(jumps[i + j], jumps[i] + 1)
-        return jumps[-1]
-
-
-# Time Limit Exceeded
-class Solution:
-    def jump(self, nums: List[int]) -> int:
-        jumps = [set() for _ in nums]
-        jumps[0].add(0)
-        for i, num in enumerate(nums):
-            for j in range(1, num + 1):
-                step = min(jumps[i])
-                if i + j < len(nums):
-                    jumps[i + j].add(step + 1)
-        return min(jumps[-1])
+        k = 0
+        prevL, prevR = 0, 0
+        while prevR < len(nums) - 1:
+            currL = prevR + 1
+            currR = max([i + nums[i] for i in range(prevL, prevR + 1)])
+            k += 1
+            prevL, prevR = currL, currR
+        return k
