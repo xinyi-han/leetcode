fbb0fa5b3825347a1c2bc44ec5c0b8768e66d1f9
Xinyi Han <xinyi.han98@outlook.com>
2023-02-18 12:48:01 +1100

    2023.02.18

--- 279-Perfect-Squares.py
+++ 279-Perfect-Squares.py
@@ -1,11 +1,12 @@
 class Solution:
     def numSquares(self, n: int) -> int:
+        maxEdge = int(n**0.5)
+        sqrs = list(map(lambda x: x**2, range(1, maxEdge + 1)))
         cache = [n for _ in range(n + 1)]
         cache[0] = 0
-        sqrs = [j**2 for j in range(1, n + 1) if j**2 <= n]
-        for i in range(1, n + 1):
-            for j in sqrs:
-                if i - j < 0:
-                    break
-                cache[i] = min(cache[i], 1 + cache[i - j])
+        for sqr in sqrs:
+            for i in range(1, n + 1):
+                if sqr > i:
+                    continue
+                cache[i] = min(cache[i], 1 + cache[i - sqr])
         return cache[-1]
