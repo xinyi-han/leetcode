1771de867df6f9be74384c5a1df1e31a5acff20b
Xinyi Han <xinyi.han98@outlook.com>
2023-02-18 12:06:26 +1100

    2023.02.18

--- 62-Unique-Paths.py
+++ 62-Unique-Paths.py
@@ -1,11 +1,7 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        grid = [[0 for _ in range(n)] for _ in range(m)]
-        for j in range(n):
-            grid[0][j] = 1
-        for i in range(m):
-            grid[i][0] = 1
-        for i in range(1, m):
-            for j in range(1, n):
-                grid[i][j] = grid[i][j - 1] + grid[i - 1][j]
-        return grid[-1][-1]
+        cache = [[1 for _ in range(n)] for _ in range(m)]
+        for i in range(m - 2, -1, -1):
+            for j in range(n - 2, -1, -1):
+                cache[i][j] = cache[i][j+1] + cache[i+1][j]
+        return cache[0][0]
