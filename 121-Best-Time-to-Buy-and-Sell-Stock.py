cfc221600526d016437d9042aa69b18723de076d
Xinyi Han <xinyi.han98@outlook.com>
2023-03-20 10:29:04 +1100

    2023.03.20

--- 121-Best-Time-to-Buy-and-Sell-Stock.py
+++ 121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -3,9 +3,12 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxProfit = 0
-        maxPrice = prices[-1]
-        for i in range(2, len(prices) + 1):
-            maxProfit = max(maxProfit, maxPrice - prices[-i])
-            maxPrice = max(maxPrice, prices[-i])
-        return maxProfit
+        l, r = 0, 1
+        profit = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = max(profit, prices[r] - prices[l])
+            else:
+                l = r
+            r += 1
+        return profit
