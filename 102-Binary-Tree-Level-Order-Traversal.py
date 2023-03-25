34139da5cd50292eee728bb6c0ada189d476e72d
Xinyi Han <xinyi.han98@outlook.com>
2023-03-25 22:23:02 +1100

    2023.03.25

--- 102-Binary-Tree-Level-Order-Traversal.py
+++ 102-Binary-Tree-Level-Order-Traversal.py
@@ -16,14 +16,15 @@ class Solution:
             return output
         queue = [root]
         while len(queue) > 0:
-            row = list()
+            level = list()
             vals = list()
             for node in queue:
                 vals.append(node.val)
                 if node.left is not None:
-                    row.append(node.left)
+                    level.append(node.left)
                 if node.right is not None:
-                    row.append(node.right)
-            output.append(vals)
-            queue = row
+                    level.append(node.right)
+            queue = level
+            if len(vals) > 0:
+                output.append(vals)
         return output
