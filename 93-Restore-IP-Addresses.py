9491d978f7e3d301ac43ed0f13e8b2f7ed772929
Xinyi Han <xinyi.han98@outlook.com>
2023-03-25 20:55:01 +1100

    2023.03.25

--- 93-Restore-IP-Addresses.py
+++ 93-Restore-IP-Addresses.py
@@ -5,23 +5,23 @@ class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         output = list()
         stack = list()
-        if len(s) < 4 or len(s) > 12:
-            return output
 
         def dfs(i: int):
             if len(stack) == 4:
                 if i == len(s):
                     output.append(".".join(stack))
                 return
-            for j in range(i, min(len(s), i + 3)):
-                section = s[i:j+1]
-                if len(section) > 1 and section[0] == "0":
+            for j in range(i, min(i + 3, len(s))):
+                address = s[i:j+1]
+                if address[0] == "0":
+                    stack.append("0")
+                    dfs(i + 1)
+                    stack.pop()
                     break
-                if len(section) == 3 and int(section) > 255:
-                    break
-                stack.append(section)
-                dfs(j + 1)
-                stack.pop()
+                if int(address) <= 255:
+                    stack.append(address)
+                    dfs(j + 1)
+                    stack.pop()
 
         dfs(0)
         return output
