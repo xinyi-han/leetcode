3b7f2cf15f839cf47311976aa31299b9d0e23f8c
Xinyi Han <xinyi.han98@outlook.com>
2023-03-25 21:18:35 +1100

    2023.03.25

--- 96-Unique-Binary-Search-Trees.py
+++ 96-Unique-Binary-Search-Trees.py
@@ -4,6 +4,8 @@ class Solution:
         cache[0] = 1
         cache[1] = 1
         for i in range(2, n + 1):
-            for j in range(i):
-                cache[i] += cache[j] * cache[i-1-j]
+            num = 0
+            for j in range(1, i + 1):
+                num += cache[j - 1] * cache[i - j]
+            cache[i] = num
         return cache[-1]
