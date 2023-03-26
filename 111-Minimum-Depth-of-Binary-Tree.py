d42a21d9ccae3815520030327c859bc08702d5d3
Xinyi Han <xinyi.han98@outlook.com>
2023-03-26 20:12:29 +1100

    2023.03.26

--- 111-Minimum-Depth-of-Binary-Tree.py
+++ 111-Minimum-Depth-of-Binary-Tree.py
@@ -11,21 +11,18 @@ class TreeNode:
 
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        self.minD = 9999
-
-        def dfs(node: TreeNode, depth: int):
-            if node.left is None and node.right is None:
-                self.minD = min(self.minD, depth)
-                return
-            elif node.left is None:
-                dfs(node.right, depth + 1)
-            elif node.right is None:
-                dfs(node.left, depth + 1)
-            else:
-                dfs(node.left, depth + 1)
-                dfs(node.right, depth + 1)
-
         if root is None:
             return 0
+        heights = set()
+
+        def dfs(node: Optional[TreeNode], h: int):
+            if node is None:
+                return
+            if node.left is None and node.right is None:
+                heights.add(h)
+                return
+            dfs(node.left, h + 1)
+            dfs(node.right, h + 1)
+
         dfs(root, 1)
-        return self.minD
+        return min(heights)
