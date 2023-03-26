364639be91c56069f021dfdfc60944b9327c8dee
Xinyi Han <xinyi.han98@outlook.com>
2023-03-26 21:57:27 +1100

    2023.03.26

--- 113-Path-Sum-II.py
+++ 113-Path-Sum-II.py
@@ -12,25 +12,20 @@ class TreeNode:
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         output = list()
+        stack = list()
 
-        def dfs(node: TreeNode, stack: List[int], sum: int):
-            val = node.val
-            stack.append(val)
+        def dfs(node: Optional[TreeNode], sum: int):
+            if node is None:
+                return
+            stack.append(node.val)
             if node.left is None and node.right is None:
-                if sum + val == targetSum:
+                if sum + node.val == targetSum:
                     output.append(list(stack))
                 stack.pop()
                 return
-            elif node.left is None:
-                dfs(node.right, stack, sum + val)
-            elif node.right is None:
-                dfs(node.left, stack, sum + val)
-            else:
-                dfs(node.left, stack, sum + val)
-                dfs(node.right, stack, sum + val)
+            dfs(node.left, sum + node.val)
+            dfs(node.right, sum + node.val)
             stack.pop()
 
-        if root is None:
-            return output
-        dfs(root, [], 0)
+        dfs(root, 0)
         return output
