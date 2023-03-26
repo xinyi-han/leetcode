bfa73df6a5eb6e4db61baae4ea638672b62b4e1e
Xinyi Han <xinyi.han98@outlook.com>
2023-03-26 23:14:35 +1100

    2023.03.26

--- 77-Combinations.py
+++ 77-Combinations.py
@@ -3,14 +3,11 @@ from typing import List
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        output = list()
-        stack = list()
+        output, stack = list(), list()
 
         def dfs(i: int):
             if len(stack) == k:
                 output.append(list(stack))
-                return
-            if i > n:
                 return
             for j in range(i, n + 1):
                 stack.append(j)
