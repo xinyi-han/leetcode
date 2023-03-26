4a58de7b8960874aca67cf5f64b17b7253a67d64
Xinyi Han <xinyi.han98@outlook.com>
2023-03-26 22:39:03 +1100

    2023.03.26

--- 46-Permutations.py
+++ 46-Permutations.py
@@ -3,18 +3,16 @@ from typing import List
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        hashMap = {num: 1 for num in nums}
         output = list()
         stack = list()
-        hashMap = dict()
-        for num in nums:
-            hashMap[num] = 1
 
         def dfs():
             if len(stack) == len(nums):
                 output.append(list(stack))
                 return
-            for num in hashMap:
-                if hashMap[num] == 1:
+            for num in nums:
+                if hashMap[num] > 0:
                     hashMap[num] -= 1
                     stack.append(num)
                     dfs()
