08f625b5a7bd804f012aaa2b88a04dc83b2e6cf4
Xinyi Han <xinyi.han98@outlook.com>
2023-03-28 13:06:09 +1100

    2023.03.28

--- 416-Partition-Equal-Subset-Sum.py
+++ 416-Partition-Equal-Subset-Sum.py
@@ -4,18 +4,19 @@ from typing import List
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
-        if total % 2 == 1:
+        if total % 2 != 0:
             return False
+        half = total // 2
         cache = dict()
 
         def dfs(i: int, sum: int) -> bool:
-            if sum == 0:
+            if sum == half:
                 return True
             if i == len(nums):
                 return False
             if (i, sum) in cache:
                 return cache[(i, sum)]
-            cache[(i, sum)] = dfs(i + 1, sum - nums[i]) or dfs(i + 1, sum)
+            cache[(i, sum)] = dfs(i + 1, sum + nums[i]) or dfs(i + 1, sum)
             return cache[(i, sum)]
 
-        return dfs(0, total // 2)
+        return dfs(0, 0)
