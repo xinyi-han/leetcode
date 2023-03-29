efe08abaa04528c8621f88d4961bc28c8d6dec84
Xinyi Han <xinyi.han98@outlook.com>
2023-03-30 10:56:13 +1100

    2023.03.30

--- 97-Interleaving-String.py
+++ 97-Interleaving-String.py
@@ -2,18 +2,18 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) != len(s3):
             return False
-        cache = [[False for _ in range(len(s2) + 1)] for _ in range(len(s1) + 1)]
-        cache[-1][-1] = True
-        for j in range(len(s2) - 1, -1, -1):
-            if s2[j:] == s3[len(s1)+j:]:
-                cache[-1][j] = True
-        for i in range(len(s1) - 1, -1, -1):
-            if s1[i:] == s3[len(s2)+i:]:
-                cache[i][-1] = True
-        for i in range(len(s1) - 1, -1, -1):
-            for j in range(len(s2) - 1, -1, -1):
-                if cache[i+1][j] and s1[i] == s3[i+j]:
-                    cache[i][j] = True
-                elif cache[i][j+1] and s2[j] == s3[i+j]:
-                    cache[i][j] = True
-        return cache[0][0]
+        cache = dict()
+
+        def dfs(i: int, j: int):
+            if i == len(s1) and j == len(s2):
+                return True
+            if (i, j) in cache:
+                return cache[(i, j)]
+            cache[(i, j)] = False
+            if i < len(s1) and s1[i] == s3[i + j]:
+                cache[(i, j)] = dfs(i + 1, j)
+            if not cache[(i, j)] and j < len(s2) and s2[j] == s3[i + j]:
+                cache[(i, j)] = dfs(i, j + 1)
+            return cache[(i, j)]
+
+        return dfs(0, 0)
