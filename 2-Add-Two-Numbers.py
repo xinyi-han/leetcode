01e4de131b463dea9f20a66e9b6b9435ba9a18de
Xinyi Han <xinyi.han98@outlook.com>
2023-06-05 20:21:57 +1000

    2023.06.05

--- 2-Add-Two-Numbers.py
+++ 2-Add-Two-Numbers.py
@@ -11,27 +11,27 @@ class ListNode:
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
-        curr = dummy
+        node = dummy
         carry = 0
         while l1 is not None and l2 is not None:
             val = l1.val + l2.val + carry
             carry = val // 10
             val %= 10
-            curr.next = ListNode(val)
-            curr = curr.next
+            node.next = ListNode(val=val)
+            node = node.next
             l1 = l1.next
             l2 = l2.next
-        if l1 is not None:
-            l = l1
-        else:
+        if l1 is None:
             l = l2
+        else:
+            l = l1
         while l is not None:
             val = l.val + carry
             carry = val // 10
             val %= 10
-            curr.next = ListNode(val)
-            curr = curr.next
+            node.next = ListNode(val=val)
+            node = node.next
             l = l.next
         if carry != 0:
-            curr.next = ListNode(carry)
+            node.next = ListNode(val=carry)
         return dummy.next
