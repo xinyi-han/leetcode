c250cfcd376f572bba13df81ca939b675e840d00
Xinyi Han <xinyi.han98@outlook.com>
2023-06-11 10:32:53 +1000

    2023.06.11

---
+++ sql/1978-Employees-Whose-Manager-Left-the-Company.sql
@@ -0,0 +1,8 @@
+SELECT e.employee_id
+FROM Employees e
+LEFT JOIN Employees m
+ON e.manager_id = m.employee_id
+WHERE e.manager_id IS NOT NULL
+AND e.salary < 30000
+AND m.employee_id IS NULL
+ORDER BY e.employee_id;
