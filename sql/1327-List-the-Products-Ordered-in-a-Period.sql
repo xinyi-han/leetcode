46dbaa7b0b4acdce493187ece6c2a0c0b9b53407
Xinyi Han <xinyi.han98@outlook.com>
2023-06-13 10:36:19 +1000

    2023.06.13

---
+++ sql/1327-List-the-Products-Ordered-in-a-Period.sql
@@ -0,0 +1,7 @@
+SELECT p.product_name, SUM(o.unit) AS unit
+FROM Products p
+INNER JOIN Orders o
+ON p.product_id = o.product_id
+WHERE o.order_date LIKE '2020-02-%'
+GROUP BY p.product_id
+HAVING SUM(o.unit) >= 100;
