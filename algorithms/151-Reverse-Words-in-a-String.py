df1d836cc3d08f4b969772f246b855b4765bf5fc
Xinyi Han <xinyi.han98@outlook.com>
2023-06-16 20:53:35 +1000

    2023.06.16

--- algorithms/151-Reverse-Words-in-a-String.py
+++ algorithms/151-Reverse-Words-in-a-String.py
@@ -1,7 +1,6 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         s = s.strip()
-        words = s.split()
-        words = [word for word in words if len(word) > 0]
-        words.reverse()
-        return " ".join(words)
+        s = s.split()
+        s.reverse()
+        return " ".join(s)
