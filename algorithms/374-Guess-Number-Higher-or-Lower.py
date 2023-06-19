dafafe4a3f51c1eb1bed1cc9a0cc7577b9449642
Xinyi Han <xinyi.han98@outlook.com>
2023-06-19 16:23:18 +1000

    2023.06.19

--- algorithms/374-Guess-Number-Higher-or-Lower.py
+++ algorithms/374-Guess-Number-Higher-or-Lower.py
@@ -9,13 +9,14 @@ def guess(num: int) -> int:
 
 class Solution:
     def guessNumber(self, n: int) -> int:
-        return self.binarySearch(1, n)
 
-    def binarySearch(self, lo: int, hi: int) -> int:
-        mid = (lo + hi) // 2
-        if guess(mid) == 0:
-            return mid
-        elif guess(mid) == 1:
-            return self.binarySearch(mid + 1, hi)
-        else:
-            return self.binarySearch(lo, mid - 1)
+        def binarySearch(lo: int, hi: int) -> int:
+            mid = lo + (hi - lo) // 2
+            if guess(mid) == 0:
+                return mid
+            elif guess(mid) == -1:
+                return binarySearch(lo, mid - 1)
+            else:
+                return binarySearch(mid + 1, hi)
+
+        return binarySearch(1, n)
