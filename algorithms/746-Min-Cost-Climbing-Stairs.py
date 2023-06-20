3356f9a637830586ad11543d570e33b65170ee49
Xinyi Han <xinyi.han98@outlook.com>
2023-06-20 11:15:41 +1000

    2023.06.20

--- algorithms/746-Min-Cost-Climbing-Stairs.py
+++ algorithms/746-Min-Cost-Climbing-Stairs.py
@@ -3,11 +3,6 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cache = [float('inf') for _ in range(len(cost) + 1)]
-        cache[0] = 0
-        cache[1] = 0
-        for i in range(len(cost)):
-            cache[i + 1] = min(cache[i + 1], cache[i] + cost[i])
-            if i < len(cost) - 1:
-                cache[i + 2] = min(cache[i + 2], cache[i] + cost[i])
-        return cache[-1]
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+        return min(cost[0], cost[1])
