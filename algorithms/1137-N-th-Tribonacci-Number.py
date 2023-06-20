a7e9dd1c7e2218da4f906564b7013b6b76a4901d
Xinyi Han <xinyi.han98@outlook.com>
2023-06-20 11:02:09 +1000

    2023.06.20

--- algorithms/1137-N-th-Tribonacci-Number.py
+++ algorithms/1137-N-th-Tribonacci-Number.py
@@ -2,13 +2,11 @@ class Solution:
     def tribonacci(self, n: int) -> int:
         cache = [0 for _ in range(n + 1)]
         if n == 0:
-            return cache[-1]
+            return 0
         cache[1] = 1
         if n == 1:
-            return cache[-1]
+            return 1
         cache[2] = 1
-        if n == 2:
-            return cache[-1]
         for i in range(3, n + 1):
             cache[i] = cache[i - 3] + cache[i - 2] + cache[i - 1]
         return cache[-1]
