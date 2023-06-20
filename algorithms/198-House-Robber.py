814b0a302a4cec2adab99454485acb4290edf46f
Xinyi Han <xinyi.han98@outlook.com>
2023-06-20 11:44:59 +1000

    2023.06.20

--- algorithms/198-House-Robber.py
+++ algorithms/198-House-Robber.py
@@ -5,6 +5,6 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         cache = [0 for _ in range(len(nums) + 1)]
         cache[1] = nums[0]
-        for i, num in enumerate(nums[1:], 2):
-            cache[i] = max(cache[i - 1], cache[i - 2] + num)
+        for i in range(2, len(cache)):
+            cache[i] = max(nums[i - 1] + cache[i - 2], cache[i - 1])
         return cache[-1]
