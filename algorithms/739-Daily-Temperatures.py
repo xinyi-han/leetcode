3860613da1ac2712104d6fa8fccd7de4644d8f72
Xinyi Han <xinyi.han98@outlook.com>
2023-06-23 13:59:43 +1000

    2023.06.23

--- algorithms/739-Daily-Temperatures.py
+++ algorithms/739-Daily-Temperatures.py
@@ -3,18 +3,11 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        output = [0 for _ in temperatures]
-        stack = list()
-        for i, temperature in enumerate(temperatures):
-            # if len(stack) == 0 or temperature <= temperatures[stack[-1]]:
-            #     stack.append(i)
-            # else:
-            #     while len(stack) > 0 and temperature > temperatures[stack[-1]]:
-            #         j = stack.pop()
-            #         output[j] = i - j
-            #     stack.append(i)
-            while len(stack) > 0 and temperature > temperatures[stack[-1]]:
+        answer = [0 for _ in temperatures]
+        stack = [0]
+        for i in range(1, len(temperatures)):
+            while len(stack) > 0 and temperatures[i] > temperatures[stack[-1]]:
                 j = stack.pop()
-                output[j] = i - j
+                answer[j] = i - j
             stack.append(i)
-        return output
+        return answer
