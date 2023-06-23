b6403c2d72667d9f10532eaeb9d23211b8a00a8c
Xinyi Han <xinyi.han98@outlook.com>
2023-06-23 16:54:06 +1000

    2023.06.23

--- algorithms/62-Unique-Paths.py
+++ algorithms/62-Unique-Paths.py
@@ -3,5 +3,5 @@ class Solution:
         cache = [[1 for _ in range(n)] for _ in range(m)]
         for i in range(m - 2, -1, -1):
             for j in range(n - 2, -1, -1):
-                cache[i][j] = cache[i][j+1] + cache[i+1][j]
+                cache[i][j] = cache[i][j + 1] + cache[i + 1][j]
         return cache[0][0]
