9d621090c88fca543c6afd85e5aea456caaf0355
Xinyi Han <xinyi.han98@outlook.com>
2023-06-26 23:12:48 +1000

    2023.06.26

--- algorithms/121-Best-Time-to-Buy-and-Sell-Stock.py
+++ algorithms/121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,24 +1,13 @@
 from typing import List
 
 
-# Time Limit Exceeded
-# class Solution:
-#     def maxProfit(self, prices: List[int]) -> int:
-#         cache = [0 for _ in prices]
-#         for i in range(len(prices) - 2, -1, -1):
-#             for j in range(i, len(prices)):
-#                 cache[i] = max(cache[i], prices[j] - prices[i])
-#         return max(cache)
-
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1
-        profit = 0
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = max(profit, prices[r] - prices[l])
-            else:
-                l = r
-            r += 1
-        return profit
+        output = 0
+        buy = prices[0]
+        for price in prices:
+            if price < buy:
+                buy = price
+            elif price > buy:
+                output = max(output, price - buy)
+        return output
